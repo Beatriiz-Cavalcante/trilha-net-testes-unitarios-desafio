9ed91d5cbe7ba86d8d828f8ca23e5708e2f6b84b
Beatriiz_Cavalcante <biamenezes193@gmail.com>
2024-01-04 14:30:36 -0300

    Desafio realizado

--- TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -5,29 +5,35 @@ namespace TestesUnitarios.Desafio.Console.Services
         public List<int> RemoverNumerosNegativos(List<int> lista)
         {
             var listaSemNegativos = lista.Where(x => x > 0);
+            //Where é usado para filtrar os elementos da lista de acordo com uma condição
             return listaSemNegativos.ToList();
+            //ToList() converte a sequência filtrada em uma nova lista
         }
 
         public bool ListaContemDeterminadoNumero(List<int> lista, int numero)
         {
             var contem = lista.Contains(numero);
+            //Contrains é usado para filtrar um caractere de uma lista e retorna um bool
             return contem;
         }
 
         public List<int> MultiplicarNumerosLista(List<int> lista, int numero)
         {
             var listaMultiplicada = lista.Select(x => x * numero).ToList();
+            //Select transforma cada elemento de uma lista de acordo com uma condição
             return listaMultiplicada;
         }
 
         public int RetornarMaiorNumeroLista(List<int> lista)
         {
             return lista.Max();
+            //Max retornar o maior elemento da sequência
         }
 
         public int RetornarMenorNumeroLista(List<int> lista)
         {
             return lista.Min();
+            //Min retornar o menor elemento da sequência
         }
     }
 }
--- TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
@@ -5,18 +5,21 @@ namespace TestesUnitarios.Desafio.Console.Services
         public int RetornarQuantidadeCaracteres(string texto)
         {
             var numeroCaracteres = texto.Length;
+            //Length é usado para contar os caracteres de uma string
             return numeroCaracteres;
         }
 
         public bool ContemCaractere(string texto, string textoProcurado)
         {
             var contem = texto.Contains(textoProcurado);
+              //Contrains é usado para filtrar um caractere de uma lista/string e retorna um bool
             return contem;
         }
 
         public bool TextoTerminaCom(string texto, string textoProcurado)
         {
             var termina = texto.EndsWith(textoProcurado);
+            //EndWith é usado para inspecionar se a string termina com o parâmetro especificado e retorna um bool
             return termina;
         }
     }
